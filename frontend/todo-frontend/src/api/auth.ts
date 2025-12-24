1fc9c2a21a701f505580f420bf4cdb99549f695d
rqrniii <razqrniii@gmail.com>
2025-12-24 10:24:16 +0300

    Fix duplicate /auth path in API calls

--- frontend/todo-frontend/src/api/auth.ts
+++ frontend/todo-frontend/src/api/auth.ts
@@ -1,10 +1,9 @@
 import authApi from "./authApi";
 
 export const register = (email: string, password: string) => {
-    return authApi.post("/auth/register", { email, password });
+    return authApi.post("/register", { email, password }); 
   };
   
-  export const login = (email: string, password: string) => {
-    return authApi.post("/auth/login", { email, password });
+export const login = (email: string, password: string) => {
+    return authApi.post("/login", { email, password });  
   };
-  
